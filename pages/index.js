e8fc51730c48f1beedc08e0060b806b62f863be8
Vincent <isfcikiller@msn.com>
2020-02-26 07:52:18 +0100

    move fluidbg to components and demo updates

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,62 @@
 import Head from 'next/head'
-import FluidAnimation from '../components/FluidBg'
+import FluidAnimationBG from '../components/FluidBg'
+import styled from 'styled-components'
+
+const SMain = styled.main`
+  position: relative;
+  z-index: 2;
+  color: #fff;
+  font-family: sans-serif;
+`
+const SSection = styled.section`
+  height: 100vh;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+const SFluidAnimationBg = styled(FluidAnimationBG)`
+  position: fixed;
+  height: 100vh;
+  left: 0;
+  top: 0;
+`
+const SFooter = styled.footer`
+  display: flex;
+  justify-content: space-between;
+  margin: 20px;
+
+  a { color: #fff; }
+
+  div:last-child {
+    a { margin: 0 10px; }
+  }
+`
 
 const Home = () => (
   <div className="container">
     <Head>
       <title>Next-JS WebGL-Fluid Scroll</title>
       <link rel="icon" href="/favicon.ico" />
-      <style>
-        {`body {
-          margin: 0;
-          height: 300vh;
-        }`}
-      </style>
     </Head>
-
-    <main>
-      <FluidAnimation
-        style={{
-          position: 'fixed',
-          height: '100vh'
-        }}
-      />
-    </main>
+    <SFluidAnimationBg />
+    <SMain>
+      <SSection>
+        <h1>This is a demo for a fluid scroll effect</h1>
+      </SSection>
+      <SSection>
+        <h1>Also works when scrolling up</h1>
+      </SSection>
+      <SFooter>
+        <div>
+          Created by <a href="https://wweb.dev/">wweb.dev/</a>
+        </div>
+        <div>
+          <a href="https://github.com/Vincenius/fluid-scroll-demo">GitHub</a>
+          <a href="https://github.com/PavelDoGreat/WebGL-Fluid-Simulation">WebGL-Fluid-Simulation</a>
+        </div>
+      </SFooter>
+    </SMain>
   </div>
 )
 
